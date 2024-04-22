134bcbc7d60fd1e9d6dd36a2e8cde788f8596d92
Bruno Santos <brsantos197@gmail.com>
2024-04-22 12:32:22 -0300

    chore: build assets

--- forge.config.ts
+++ forge.config.ts
@@ -12,16 +12,16 @@ import fs from 'node:fs'
 
 const config: ForgeConfig = {
   hooks: {
-    // postMake: async (ctx, makeResult) => {
-    //   for (const makeTarget of makeResult) {
-    //     makeTarget.artifacts.forEach((artifact, index) => {
-    //       if (index !== 0) {
-    //         fs.renameSync(artifact, artifact.split(makeTarget.packageJSON.version).join(`${makeTarget.packageJSON.version}_${makeTarget.arch}`))
-    //       }
-    //     });
-    //   }
-    //   return makeResult
-    // }
+    postMake: async (ctx, makeResult) => {
+      for (const makeTarget of makeResult) {
+        makeTarget.artifacts.forEach((artifact, index) => {
+          if (index !== 0) {
+            fs.renameSync(artifact, artifact.split(makeTarget.packageJSON.version).join(`${makeTarget.packageJSON.version}_${makeTarget.arch}`))
+          }
+        });
+      }
+      return makeResult
+    }
   },
   packagerConfig: {
     asar: true,
